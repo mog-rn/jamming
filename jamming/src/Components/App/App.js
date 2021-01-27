86137455cac9fb8c2b80f34e2a02dd861155442a
Amos Mogaka <mogaka.amo254@gmail.com>
2021-01-27 23:16:21 +0300

    Commit

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -3,18 +3,7 @@ import './App.css';
 import { render } from 'react-dom';
 
 function App() {
-  render () {
-    return (
-      <div>
-  <h1>Ja<span class="highlight">mmm</span>ing</h1>
-  <div class="App">
-    <div class="App-playlist">
-      
-    </div>
-  </div>
-</div>
-      )
-  }
+  
 }
 
 export default App;
