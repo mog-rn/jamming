9569849e01051460b5b30af52b3efa99e01ca917
Amos Mogaka <mogaka.amo254@gmail.com>
2021-01-28 10:02:54 +0300

    Commit

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -9,8 +9,10 @@ class App extends React.Component {
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
+                    /**Add a searchResults component */
                     <div className="App-playlist">
-
+                        /**Add a searchResults components */
+                        /**Add a playlist component */
                     </div>
                 </div>
             </div>
