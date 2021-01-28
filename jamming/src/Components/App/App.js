7f57f823aa7c37e03994b09a803c217477c2a06e
Amos Mogaka <mogaka.amo254@gmail.com>
2021-01-28 09:55:48 +0300

    Rendered the HTML in App.js

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import { render } from 'react-dom';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import ReactDOM, { } from 'react-dom'
 
-function App() {
-  
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Ja<span class="highlight">mmm</span>ing</h1>
+                <div class="App">
+                    <div class="App-playlist">
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
