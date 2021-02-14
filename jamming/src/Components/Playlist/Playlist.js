afc8d769ab9048d445d4217feb7249d1aa5bd839
Mogakamo <mogaka.amo254@gmail.com>
2021-02-14 17:43:07 +0300

    Edits

--- jamming/src/Components/Playlist/Playlist.js
+++ jamming/src/Components/Playlist/Playlist.js
@@ -5,7 +5,7 @@ class Playlist extends React.Component {
         return (
             <div className="Playlist">
                 <input value="New Playlist" />
-                /**Add a TrackList.component */
+                {/**Add a TrackList.component */}
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )}
